ca9ca149a84b4cbf24ff469ca5bc10cc4dcaf0ab
Pratik <pratikmore6588@gmail.com>
2024-01-20 22:50:41 +0530

    Merge remote-tracking branch 'origin/master'

--- Basics/src/binary_search.java
+++ Basics/src/binary_search.java
@@ -9,7 +9,7 @@ public class binary_search {
     static int search(int[] arr, int target){
         int start = 0;
         int end = arr.length-1;
-        while (start < end){
+        while (start <= end){
             int mid = start + (end - start)/2;
 
             if (target < arr[mid]){
