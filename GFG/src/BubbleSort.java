2442b3c4117affe8c8eec299d44e2c57ad44a1f7
Pratik <pratikmore6588@gmail.com>
2024-02-22 23:28:27 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ GFG/src/BubbleSort.java
@@ -0,0 +1,47 @@
+class BubbleSort
+{
+    //Function to sort the array using bubble sort algorithm.
+    public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n - i; j++) {
+                if (arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+//    Bubble Sort
+//
+//
+//    Given an Integer N and a list arr. Sort the array using bubble sort algorithm.
+//        Example 1:
+//
+//        Input:
+//        N = 5
+//        arr[] = {4, 1, 3, 9, 7}
+//        Output:
+//        1 3 4 7 9
+//        Example 2:
+//
+//        Input:
+//        N = 10
+//        arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+//        Output:
+//        1 2 3 4 5 6 7 8 9 10
+//
+//        Your Task:
+//        You don't have to read input or print anything. Your task is to complete the function bubblesort() which takes the array and it's size as input and sorts the array using bubble sort algorithm.
+//
+//        Expected Time Complexity: O(N^2).
+//        Expected Auxiliary Space: O(1).
+//
+//        Constraints:
+//        1 <= N <= 103
+//        1 <= arr[i] <= 103
